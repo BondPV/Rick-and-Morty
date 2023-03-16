69362508912669e350309c938488454c7efe6f51
BondPV <bondpv@yandex.ru>
2023-03-16 13:48:44 +0300

    feat: implement save to local storage

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
+import { getStorage, setStorage, StorageKey } from '../../utils/localStorage';
 import styles from './Search.module.scss';
 
 class Search extends React.Component<{ search: (str: string) => void }, { search?: string }> {
   state = {
-    search: '',
+    search: getStorage(StorageKey.search) || '',
+  };
+
+  componentDidMount() {
+    setStorage(StorageKey.search, this.state.search);
+  }
+
+  componentDidUpdate() {
+    setStorage(StorageKey.search, this.state.search);
+  
+
+  searchInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ search: event.target.value });
+    setStorage(StorageKey.search, this.state.search);
   };
 
   handleKey = (event: React.KeyboardEvent<HTMLElement>) => {
     if (event.key === 'Enter') {
       this.props.search(this.state.search);
+      setStorage(StorageKey.search, this.state.search);
     }
   };
 
@@ -20,7 +35,7 @@ class Search extends React.Component<{ search: (str: string) => void }, { search
           type="search"
           placeholder="search"
           value={this.state.search}
-          onChange={(e) => this.setState({ search: e.target.value })}
+          onChange={this.searchInputChange}
           onKeyDown={this.handleKey}
         />
         <button className={styles.search__btn} onClick={() => this.props.search(this.state.search)}>
