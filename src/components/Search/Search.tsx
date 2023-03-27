d75bd8be66897928cbf84cb4803fcf06cfff9ea5
BondPV <bondpv@yandex.ru>
2023-03-27 16:13:04 +0300

    refactor: changed search component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,53 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { getStorage, setStorage, StorageKey } from '../../utils/localStorage';
 import styles from './Search.module.scss';
 
-class Search extends React.Component<{ searchCards: (str: string) => void }, { search?: string }> {
-  public state = {
-    search: getStorage(StorageKey.search) || '',
+interface ISearchProps {
+  searchCards: (str: string) => void;
+}
+
+const Search = ({ searchCards }: ISearchProps) => {
+  const [search, setSearch] = useState(() => getStorage(StorageKey.search) || '');
+
+  const searchInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setSearch(event.target.value);
+    setStorage(StorageKey.search, search);
   };
 
-  public componentDidMount() {
-    this.props.searchCards(this.state.search);
-    setStorage(StorageKey.search, this.state.search);
-  }
-
-  public componentDidUpdate() {
-    setStorage(StorageKey.search, this.state.search);
-  }
-
-  private searchInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ search: event.target.value });
-    setStorage(StorageKey.search, this.state.search);
-  };
-
-  private handleKey = (event: React.KeyboardEvent<HTMLElement>) => {
+  const handleKey = (event: React.KeyboardEvent<HTMLElement>): void => {
     if (event.key === 'Enter') {
-      this.props.searchCards(this.state.search);
-      setStorage(StorageKey.search, this.state.search);
+      searchCards(search);
+      setStorage(StorageKey.search, search);
     }
   };
 
-  public render() {
-    return (
-      <div className={styles.search}>
-        <input
-          className={styles.search__input}
-          type="search"
-          placeholder="search"
-          value={this.state.search}
-          onChange={this.searchInputChange}
-          onKeyDown={this.handleKey}
-        />
-        <button
-          className={styles.search__btn}
-          onClick={() => this.props.searchCards(this.state.search)}
-        >
-          Search
-        </button>
-      </div>
-    );
-  }
-}
+  useEffect(() => {
+    setStorage(StorageKey.search, search);
+  }, [search]);
+
+  return (
+    <div className={styles.search}>
+      <input
+        className={styles.search__input}
+        type="search"
+        placeholder="search"
+        value={search}
+        onChange={searchInputChange}
+        onKeyDown={handleKey}
+      />
+      <button className={styles.search__btn} onClick={() => searchCards(search)}>
+        Search
+      </button>
+    </div>
+  );
+};
 
 export { Search };
