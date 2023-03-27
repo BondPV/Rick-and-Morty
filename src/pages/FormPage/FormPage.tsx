db96492737a503a5a121ef04fab9767bea25f9e6
BondPV <bondpv@yandex.ru>
2023-03-27 19:55:58 +0300

    refactor: changed form page

--- src/pages/FormPage/FormPage.tsx
+++ src/pages/FormPage/FormPage.tsx
@@ -1,50 +1,31 @@
-import React from 'react';
+import { useState } from 'react';
 import { Alert } from '../../components/Alert/Alert';
 import { Form } from '../../components/Form/Form';
 import { FormCards } from '../../components/FormCards/FormCards';
 import { GLOBAL_STYLES } from '../../constants/Constants';
 import { IFormCard } from '../../types/interfaces';
 
-interface IFormPageState {
-  cards?: IFormCard[];
-  isShowAlert: boolean;
-}
-
 const alertMessage = 'Card created successfully';
 
-class FormPage extends React.Component<{}, IFormPageState> {
-  public state = {
-    cards: [],
-    isShowAlert: false,
+const FormPage = () => {
+  const [cards, setCards] = useState<IFormCard[]>([]);
+  const [showAlert, setShowAlert] = useState(false);
+
+  const addCard = (card: IFormCard) => {
+    setCards([...cards, card]);
   };
 
-  private addCard = (card: IFormCard) => {
-    const updateCards: IFormCard[] = [...this.state.cards];
-    updateCards.push(card);
-    this.setState({ cards: updateCards });
+  const handleAlert = (show: boolean) => {
+    show ? setShowAlert(true) : setShowAlert(false);
   };
 
-  private setIsShowAlert = (show: boolean) => {
-    show ? this.setState({ isShowAlert: true }) : this.setState({ isShowAlert: false });
-  };
-
-  public render() {
-    return (
-      <div className={GLOBAL_STYLES.CONTAINER}>
-        <Form card={this.addCard} setIsShowAlert={this.setIsShowAlert} />
-        {this.state.isShowAlert ? (
-          <Alert
-            message={alertMessage}
-            isShow={this.state.isShowAlert}
-            setIsShow={this.setIsShowAlert}
-          />
-        ) : (
-          ''
-        )}
-        <FormCards cards={this.state.cards} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className={GLOBAL_STYLES.CONTAINER}>
+      <Form card={addCard} setIsShowAlert={handleAlert} />
+      {showAlert ? <Alert message={alertMessage} isShow={showAlert} setIsShow={handleAlert} /> : ''}
+      <FormCards cards={cards} />
+    </div>
+  );
+};
 
 export { FormPage };
