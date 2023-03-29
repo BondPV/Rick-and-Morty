64d8b93d6f97bf2cc6ebb8e359a1f99a7fced8a1
BondPV <bondpv@yandex.ru>
2023-03-29 16:44:07 +0300

    fix: fix form tests

--- src/components/Form/InputValue/InputValue.test.tsx
+++ src/components/Form/InputValue/InputValue.test.tsx
@@ -1,9 +1,19 @@
 import { render, screen } from '@testing-library/react';
+import { FormProvider, useForm } from 'react-hook-form';
 import { InputValue } from './InputValue';
 
 describe('InputValue', () => {
   test('should render input value component', () => {
-    render(<InputValue title="Test title" type="text" name="test" />);
+    const TestInputValue = () => {
+      const methods = useForm();
+      return (
+        <FormProvider {...methods}>
+          <InputValue title="Test title" name="test" type="text" />
+        </FormProvider>
+      );
+    };
+
+    render(<TestInputValue />);
 
     expect(screen.getByText('Test title')).toBeInTheDocument();
   });
