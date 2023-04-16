2139f1e4b7a81814e79407b81039e08338640a15
BondPV <bondpv@yandex.ru>
2023-04-16 14:59:05 +0300

    fix: fix form cards test

--- src/utils/localStorage.ts
+++
@@ -1,9 +0,0 @@
-enum StorageKey {
-  search = 'search',
-}
-
-const setStorage = (key: StorageKey, value: string): void => localStorage.setItem(key, value);
-
-const getStorage = (key: StorageKey): string | null => localStorage.getItem(key);
-
-export { StorageKey, setStorage, getStorage };
