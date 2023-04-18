effb64c378d6092c454404c66bc9d35e89d340a5
BondPV <bondpv@yandex.ru>
2023-04-18 08:15:21 +0300

    feat: add vite ssr server

---
+++ server.ts
@@ -0,0 +1,52 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import express, { Request, Response, NextFunction } from 'express';
+import { createServer as createViteServer, ViteDevServer } from 'vite';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const PORT = 5173;
+
+const createServer = async (): Promise<void> => {
+  const app = express();
+
+  const vite: ViteDevServer = await createViteServer({
+    server: { middlewareMode: true },
+    appType: 'custom',
+  });
+
+  app.use(vite.middlewares);
+
+  app.use('*', async (req: Request, res: Response, next: NextFunction) => {
+    const url: string = req.originalUrl;
+
+    try {
+      let template: string = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf-8');
+      template = await vite.transformIndexHtml(url, template);
+      const html = template.split('<!--ssr-outlet-->');
+      const { render } = await vite.ssrLoadModule('/src/entry-server.ts');
+      const { pipe }: { pipe: (res: Response) => void } = await render(url, {
+        onShellReady(): void {
+          res.write(html[0]);
+          pipe(res);
+        },
+        onAllReady(): void {
+          res.write(html[1]);
+          res.end();
+        },
+      });
+
+      res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
+    } catch (e: unknown) {
+      if (e instanceof Error) {
+        vite.ssrFixStacktrace(e);
+        next(e);
+      }
+    }
+  });
+
+  app.listen(PORT, () => console.log('Server started at http://localhost:' + PORT));
+};
+
+createServer();
