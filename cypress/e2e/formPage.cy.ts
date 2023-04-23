76151641ea1cfdc6f1f27a5256dbe3fda1b4f98f
BondPV <bondpv@yandex.ru>
2023-04-23 21:49:59 +0300

    fix: delete comments

--- cypress/e2e/formPage.cy.ts
+++ cypress/e2e/formPage.cy.ts
@@ -11,24 +11,20 @@ describe('Form Page', () => {
   });
 
   it('should displays the form field', () => {
-    // cy.visit('/form');
     cy.get('form');
     cy.get('button').contains('Create');
   });
 
   it('should displays message "No cards created"', () => {
-    // cy.visit('/form');
     cy.get('h3').should('have.text', 'No cards created');
   });
 
   it('should display error message when the form is not completed', () => {
-    // cy.visit('/form');
     cy.get('button').contains('Create').click();
     cy.get('[class*=form] [class^=_error]').should('have.length', 6);
   });
 
   it('should be created card', () => {
-    // cy.visit('/form');
     cy.get('#name').type('Test Name');
     cy.get('select[name="location"]').select('Earth (C-137)');
     cy.get('#created').type('2023-04-19');
@@ -43,8 +39,4 @@ describe('Form Page', () => {
 
     cy.get('[data-testid="Test Name"]');
   });
-
-  // it('Just a test to remove page load on coverage saving', () => {
-  //   expect(true).to.equal(true);
-  // });
 });
